84f570db8321388060e3299daa01bc4a1c2d8188
kickbell <dev.kickbell@gmail.com>
2022-07-25 22:47:57 +0900

    Initial commit, Add Storyboard

---
+++ POP/POP/Views/ToDoItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoItemTableViewCell.swift
+//  POP
+//
+//  Created by jc.kim on 7/25/22.
+//
+
+import UIKit
+
+class ToDoItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
