44d01ba4a272cd59aac0a15bf885de767fd31fe7
kickbell <dev.kickbell@gmail.com>
2022-07-25 22:51:09 +0900

    Add Basic Function

--- POP/POP/Views/AddItemViewController.swift
+++ POP/POP/Views/AddItemViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class AddItemViewController: UIViewController {
+    var createdItem: ((String, Date) -> Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        itemTitle.becomeFirstResponder()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    @IBOutlet var itemTitle: UITextView!
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onDone() {
+        createdItem?(itemTitle.text ?? "", Date())
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
--- POP/POP/Views/ToDoListViewController.swift
+++ POP/POP/Views/ToDoListViewController.swift
@@ -8,82 +8,40 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
+    var todoItems: [(title: String, createdAt: Date)] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "CreateNewItem", let toVC = segue.destination as? AddItemViewController {
+            toVC.createdItem = { [weak self] title, createdAt in
+                self?.todoItems.append((title, createdAt))
+                self?.tableView.reloadData()
+            }
+        }
     }
+}
 
+extension ToDoListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        todoItems.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath) as? ToDoItemTableViewCell else {
+            fatalError("tableViewCell has not dequeued!")
+        }
 
-        // Configure the cell...
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd HH:mm:ss"
+
+        let item = todoItems[indexPath.row]
+        cell.itemTitle.text = item.title
+        cell.updatedAt.text = formatter.string(from: item.createdAt)
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
